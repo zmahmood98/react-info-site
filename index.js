f4dc403483878324424a7d3a8e093ad2def29587
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-27 19:06:54 +0000

    add main content rendering function

--- index.js
+++ index.js
@@ -51,6 +51,15 @@ function Navbar() {
         </nav>
     )
 }
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+function MainContent() {
+    return(
+        <h1>I'm learning React!</h1>
+    )
+}
 
 ReactDOM.render(
     <div>
