ef46b7deaef4be4498279f3e5ecda872f71f0a14
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-27 14:25:44 +0000

    render ul tag with react

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // render h2 tag inside the root div
 ReactDOM.render(<h2>Hello, everyone!</h2>, document.getElementById("root"))
+
+// render a <ul> with 2+ <li>s inside
+ReactDOM.render(
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+    </ul>, 
+    document.getElementById("root")
+)
