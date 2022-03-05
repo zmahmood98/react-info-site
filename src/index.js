bd15b30fbca5737288ae1bc5b41040b9fdf36c95
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-05 14:25:09 +0000

    update main content

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// render h2 tag inside the root div
+/* ReactDOM.render(<h2>Hello, everyone!</h2>, document.getElementById("root"))
+*/
+
+// render a <ul> with 2+ <li>s inside
+/* ReactDOM.render(
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+    </ul>, 
+    document.getElementById("root")
+)
+*/
+
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// function MainContent() {
+//     return(
+//         <h1>I'm learning React!</h1>
+//     )
+// }
+
+// Afterwards, render it below the Navbar
+
+// ReactDOM.render(
+//     <div>
+//         <p>Hi</p>
+//         <Navbar />
+//         <MainContent />
+//     </div>,
+//     document.getElementById("root")
+// )
+
+
